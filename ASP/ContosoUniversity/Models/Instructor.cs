23d30b0686d7518abc5a296a6368a37230757959
Александр Архипов <liney13@mail.ru>
2024-11-02 20:26:09 +0800

    New Entyties DONE

---
+++ ASP/ContosoUniversity/Models/Instructor.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Contracts;
+
+namespace ContosoUniversity.Models;
+
+public class Instructor
+{
+    public int ID { get; set; }
+    [Required]
+    [Display(Name ="Фамилия")]
+    [StringLength(50)]
+    [RegularExpression("^[A-ZА-Я]+[a-zа-я]*$")]
+    public string LastName { get; set; }
+
+    [Required]
+    [Display(Name = "Имя")]
+    [StringLength(50)]
+    [RegularExpression("^[A-ZА-Я]+[a-zа-я]*$")]
+    [Column("FirstName")]//свойство FirstMidName связано с полем таблицы БД "FirstName"
+    public string FirstMidName { get; set; }
+
+    [Display(Name ="Дата трудоустройства")]
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)]
+    public DateTime HireDate { get; set; }
+    public string FullName
+    {
+        get=>$"{LastName} {FirstMidName}";
+    }
+    //Navigation property:
+    public ICollection<Course> Courses { get; set; }
+    public OfficeAssigment OfficeAssigment { get; set; }
+}
--- ASP/ContosoUniversity/Models/Student.cs
+++ ASP/ContosoUniversity/Models/Student.cs
@@ -1,11 +1,34 @@
-﻿namespace ContosoUniversity.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models;
 
 public class Student
 {
     public int ID { get; set; }
+    [Required]
+    [StringLength(50, ErrorMessage = "Too many sympols for Last Name")]
+    [RegularExpression(@"^[A-ZА-Я]+[a-zа-я]*$")]
+    [Display(Name = "Фамилия")]
     public string LastName { get; set; }
+    [Required]
+    [StringLength(50,ErrorMessage="Too many sympols for First Name")]
+    [RegularExpression(@"^[A-ZА-Я]+[a-zа-я]*$")]
+    [Column("first_name")]//свойство связано с полем таблицы БД "first_name"
+    [Display(Name = "Имя")]
     public string FirstName { get; set; }
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)]
+    [Display(Name = "Дата поступления")]
     public DateTime EnrollmentDate { get; set; }
+    [Display(Name ="Полное имя")]
+    public string FullName //Calculated Property
+    {
+        get=>$"{LastName} {FirstName}";
+    }
     //Navigation Properties:
     public ICollection<Enrollment> Enrollments { get; set; }
 }
