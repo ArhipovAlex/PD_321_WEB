9a044ec110d56fda2895689caa073f5f6cb18118
Александр Архипов <liney13@mail.ru>
2024-11-12 21:29:41 +0800

    students groups & directions DONE

---
+++ ASP/Academy/Models/Direction.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy.Models;
+
+public class Direction
+{
+    [Key] 
+    public byte direction_id {get; set; }
+    [Required]
+    public required string direction_name {get; set; }
+
+    public ICollection<Group>? Groups { get; set; }
+
+}
---
+++ ASP/Academy/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Academy.Models;
+
+public class Student
+{
+    [Key]
+    public int stud_id { get; set; }
+    [Required]
+    public required string last_name { get; set; }
+    public required string first_name { get; set; }
+    public string? middle_name { get; set; }
+    [DataType(DataType.Date)]
+    public required DateOnly birth_date { get; set; }
+    [Required]
+    [ForeignKey("Group")]
+    public required int group {  get; set; }
+    public required Group Group { get; set; }
+}
