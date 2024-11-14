9280118fb7d90d0480582fe0619a63ff6351a92c
Александр Архипов <liney13@mail.ru>
2024-11-14 19:05:34 +0800

    teacher page add

---
+++ ASP/Academy/Models/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy.Models;
+
+public class Teacher
+{
+    [Key]
+    public int teacher_id { get; set; }
+    [Required]
+    public required string last_name { get; set; }
+    public required string first_name { get; set; }
+    public string? middle_name { get; set; }
+    [DataType(DataType.Date)]
+    public required DateOnly birth_date { get; set; }
+    public required DateOnly work_since { get; set; }
+
+}
