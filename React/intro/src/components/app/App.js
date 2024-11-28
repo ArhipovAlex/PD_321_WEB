6f4c90fe0ccea178a39093e59ebbb70a1cac4098
Александр Архипов <liney13@mail.ru>
2024-11-28 21:07:27 +0800

    Navigation

--- React/intro/src/components/app/App.js
+++ React/intro/src/components/app/App.js
@@ -2,13 +2,22 @@
 import './App.css';
 import Arcticle from '../arcticle/Arcticle';
 import Heazer from '../heazer/Heazer';
-import Footer from '../footer/Footer';
+import Nav from '../nav/Nav';
 import Power from '../power/Power';
+import Footer from '../footer/Footer';
 
 function App() {
+  let nav ={
+    "Главная":"/index",
+    "Новости":"/news",
+    "Магазин":"/shop",
+    "О нас":"/about",
+    "Контакты":"/contacts"
+  }
   return (
     <div className="App">
       <Heazer title="Hello React" description="This is my first React App"/>
+      <Nav navigation={nav}/>
       <Power a={2} n={8}/>
       <Arcticle/>
       <Arcticle/>
